7a4dab8c318706a5b59c9e41474ed7241e8e2c17
AndrewGWMHS <goff.614615@swl.k12.oh.us>
2024-10-03 08:45:03 -0400

    hi

--- src/FuelCosts.java
+++ src/FuelCosts.java
@@ -1,5 +1,81 @@
-public class Main {
+import java.util.Scanner;
+
+public class FuelCosts {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        double gallons = 0;
+        double efficiency = 0;
+        double pricePerGallon = 0;
+        boolean validGallons = false;
+        boolean validEfficiency = false;
+        boolean validPrice = false;
+        double costOfTotal;
+        double distance;
+
+        do {
+            System.out.println("Enter the number of gallons of gasoline");
+            if (scan.hasNextDouble()) {
+                gallons = scan.nextDouble();
+                if (gallons >= 0) {
+                    validGallons = true;
+                }
+                else {
+                    scan.nextLine();
+                    System.out.println("Invalid number of gallons, try again");
+                    System.out.println();
+                }
+            }
+            else {
+                scan.nextLine();
+                System.out.println("Invalid data type, try again");
+                System.out.println();
+            }
+        } while(!validGallons);
+
+        do {
+            System.out.println("Enter the efficiency in miles per gallons");
+            if (scan.hasNextDouble()) {
+                efficiency = scan.nextDouble();
+                if (efficiency >= 0) {
+                    validEfficiency = true;
+                }
+                else {
+                    scan.nextLine();
+                    System.out.println("Invalid efficiency value, try again");
+                    System.out.println();
+                }
+            }
+            else {
+                scan.nextLine();
+                System.out.println("Invalid data type, try again");
+                System.out.println();
+            }
+        } while(!validEfficiency);
+
+        do {
+            System.out.println("Enter the current price for gallons of gas");
+            if (scan.hasNextDouble()) {
+                pricePerGallon = scan.nextDouble();
+                if (pricePerGallon >= 0) {
+                    validPrice = true;
+                }
+                else {
+                    scan.nextLine();
+                    System.out.println("Invalid price value, try again");
+                    System.out.println();
+                }
+            }
+            else {
+                scan.nextLine();
+                System.out.println("Invalid data type, try again");
+                System.out.println();
+            }
+        } while(!validPrice);
+
+        costOfTotal = pricePerGallon/efficiency * 100;
+        distance = gallons*efficiency;
+
+        System.out.printf("Cost per 100 miles: %8.2f", costOfTotal);
+        System.out.printf("\nCost per 100 miles: %8.2f", distance);
     }
 }
